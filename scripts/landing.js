66a11f7ebd9890080e6c0e3813e995c5bd946e36
Brandon Kelly <brandonmichaelkelly@gmail.com>
2017-03-08 19:50:16 -0700

    added rotation animiation to style points

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,9 +4,9 @@ var animatePoints = function() {
 	
 	var revealPoint = function(number) {
 		points[number].style.opacity = 1;
-		points[number].style.transform = "scaleX(1) translateY(0) rotate(180deg)";
-		points[number].style.msTransform = "scaleX(1) translateY(0) rotate(180deg)";
-		points[number].style.WebkitTransform = "scaleX(1) translateY(0) rotate(180deg)";	
+		points[number].style.transform = "scaleX(1) translateY(0) rotate(360deg)";
+		points[number].style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
+		points[number].style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)";
 	}
 	
 	for (var i = 0; i < points.length; i++) {
